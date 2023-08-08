121825a1b88eaa108dfa6ad0249bc00f860fc69a
Jim Pivarski <jpivarski@gmail.com>
2023-08-08 16:46:06 -0500

    Implemented most methods for PrimitiveArray and ListOffsetArray.

--- src/AwkwardArray.jl
+++ src/AwkwardArray.jl
@@ -1,13 +1,104 @@
 module AwkwardArray
 
-abstract type AwkwardContent end
+abstract type Content end
 
-struct PrimitiveArray{T} <: AwkwardContent
-    data::AbstractArray{T, 1}
+function Base.iterate(x::Content)
+    start = firstindex(x)
+    stop = lastindex(x)
+    if stop >= start
+        x[start], start + 1
+    else
+        nothing
+    end
+end
+
+function Base.iterate(x::Content, state)
+    stop = lastindex(x)
+    if stop >= state
+        x[state], state + 1
+    else
+        nothing
+    end
+end
+
+### PrimitiveArray #######################################################
+
+struct PrimitiveArray{T} <: Content
+    data::AbstractArray{T,1}
+end
+
+function is_valid(x::PrimitiveArray)
+    true
 end
 
 function Base.length(x::PrimitiveArray)
     Base.length(x.data)
+end
+
+function Base.firstindex(x::PrimitiveArray)
+    Base.firstindex(x.data)
+end
+
+function Base.lastindex(x::PrimitiveArray)
+    Base.lastindex(x.data)
+end
+
+function Base.getindex(x::PrimitiveArray, i::Int)
+    x.data[i]
+end
+
+function Base.getindex(x::PrimitiveArray, r::UnitRange{Int})
+    PrimitiveArray(x.data[(r.start):(r.stop)])
+end
+
+function Base.:(==)(x::PrimitiveArray, y::PrimitiveArray)
+    x.data == y.data
+end
+
+### ListOffsetArray ######################################################
+
+struct ListOffsetArray{T<:Union{Int32,UInt32,Int64}} <: Content
+    offsets::AbstractArray{T,1}
+    content::Content
+end
+
+function is_valid(x::ListOffsetArray)
+    true
+end
+
+function Base.length(x::ListOffsetArray)
+    Base.length(x.offsets) - 1
+end
+
+function Base.firstindex(x::ListOffsetArray)
+    Base.firstindex(x.offsets)
+end
+
+function Base.lastindex(x::ListOffsetArray)
+    Base.lastindex(x.offsets) - 1
+end
+
+function Base.getindex(x::ListOffsetArray, i::Int)
+    start = x.offsets[i] + Base.firstindex(x.content)
+    stop = x.offsets[i+1] + Base.firstindex(x.content) - 1
+    x.content[start:stop]
+end
+
+function Base.getindex(x::ListOffsetArray, r::UnitRange{Int})
+    ListOffsetArray(x.offsets[(r.start):(r.stop+1)], x.content)
+end
+
+function Base.:(==)(x::ListOffsetArray, y::ListOffsetArray)
+    if length(x) != length(y)
+        return false
+    else
+        for (xi, yi) in zip(x, y)
+            if xi != yi
+                return false
+            end
+        end
+        return true
+    end
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,41 @@ using AwkwardArray
 using Test
 
 @testset "AwkwardArray.jl" begin
-    layout_PrimitiveArray = AwkwardArray.PrimitiveArray([1.1, 2.2, 3.3, 4.4, 5.5])
+    begin
+        layout = AwkwardArray.PrimitiveArray([1.1, 2.2, 3.3, 4.4, 5.5])
+        @test AwkwardArray.is_valid(layout)
+        @test length(layout) == 5
+        @test layout[2] == 2.2
+        @test layout[end] == 5.5
+        @test layout[end-1] == 4.4
+        @test layout[2:4] == AwkwardArray.PrimitiveArray([2.2, 3.3, 4.4])
+        tmp = 0.0
+        for x in layout
+            @test x < 6
+            tmp += x
+        end
+        @test tmp == 16.5
+    end
 
-    @test length(layout_PrimitiveArray) == 5
+    begin
+        layout = AwkwardArray.ListOffsetArray(
+            [0, 3, 3, 5],
+            AwkwardArray.PrimitiveArray([1.1, 2.2, 3.3, 4.4, 5.5]),
+        )
+        @test AwkwardArray.is_valid(layout)
+        @test length(layout) == 3
+        @test layout[1] == AwkwardArray.PrimitiveArray([1.1, 2.2, 3.3])
+        @test layout[end-1] == AwkwardArray.PrimitiveArray([])
+        @test layout[end] == AwkwardArray.PrimitiveArray([4.4, 5.5])
+        @test layout[1:2] == AwkwardArray.ListOffsetArray(
+            [0, 3, 3],
+            AwkwardArray.PrimitiveArray([1.1, 2.2, 3.3]),
+        )
+        tmp = 0
+        for x in layout
+            @test length(x) <= 3
+            tmp += length(x)
+        end
+        @test tmp == 5
+    end
 end
