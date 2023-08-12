2005da80cc4f4bed02786c8d9ea69059599741ee
Jim Pivarski <jpivarski@users.noreply.github.com>
2023-08-11 19:26:34 -0500

    feat: implemented RecordArray (along with Record scalar). (#11)
    * feat: implemented RecordArray (along with Record scalar).
    
    * Filling RecordArray as a LayoutBuilder.
    
    * Layout equality should check type parameters: https://github.com/scikit-hep/awkward/blob/e2e5df69916696e85331473fe156aa1af75e88a3/src/awkward/_parameters.py#L8-L13.
    
    * Added copy(::RecordArray) and fixed equality check.

--- src/AwkwardArray.jl
+++ src/AwkwardArray.jl
@@ -61,8 +61,27 @@ get_parameter(parameters::Parameters, key::String) =
         nothing
     end
 
+function compatible(parameters1::Parameters, parameters2::Parameters)
+    if get(parameters1.string_valued, "__array__", "") !=
+       get(parameters2.string_valued, "__array__", "")
+        return false
+    end
+    if get(parameters1.string_valued, "__list__", "") !=
+       get(parameters2.string_valued, "__list__", "")
+        return false
+    end
+    if get(parameters1.string_valued, "__record__", "") !=
+       get(parameters2.string_valued, "__record__", "")
+        return false
+    end
+    return true
+end
+
 Base.length(parameters::Parameters) =
     length(parameters.string_valued) + length(parameters.any_valued)
+
+Base.keys(parameters::Parameters) =
+    union(keys(parameters.any_valued), keys(parameters.string_valued))
 
 Base.show(io::IO, parameters::Parameters) = print(
     io,
@@ -152,15 +171,18 @@ Base.length(layout::PrimitiveArray) = length(layout.data)
 Base.firstindex(layout::PrimitiveArray) = firstindex(layout.data)
 Base.lastindex(layout::PrimitiveArray) = lastindex(layout.data)
 
-function Base.getindex(layout::PrimitiveArray, i::Int)
-    layout.data[i]
-end
+Base.getindex(layout::PrimitiveArray, i::Int) = layout.data[i]
 
-function Base.getindex(layout::PrimitiveArray, r::UnitRange{Int})
-    PrimitiveArray(layout.data[r])
-end
+Base.getindex(layout::PrimitiveArray, r::UnitRange{Int}) = PrimitiveArray(
+    layout.data[r],
+    parameters = layout.parameters,
+    behavior = typeof(layout).parameters[end],
+)
 
 function Base.:(==)(layout1::PrimitiveArray, layout2::PrimitiveArray)
+    if !compatible(layout1.parameters, layout2.parameters)
+        return false
+    end
     layout1.data == layout2.data
 end
 
@@ -187,12 +209,8 @@ end
 ListOffsetArray{INDEX,CONTENT}(;
     parameters::Parameters = Parameters(),
     behavior::Symbol = :default,
-) where {INDEX<:IndexBig} where {CONTENT<:Content} = AwkwardArray.ListOffsetArray(
-    INDEX([0]),
-    CONTENT(),
-    parameters = parameters,
-    behavior = behavior,
-)
+) where {INDEX<:IndexBig} where {CONTENT<:Content} =
+    ListOffsetArray(INDEX([0]), CONTENT(), parameters = parameters, behavior = behavior)
 
 function copy(
     layout::ListOffsetArray{INDEX,CONTENT,BEHAVIOR};
@@ -228,7 +246,7 @@ function is_valid(layout::ListOffsetArray)
             return false
         end
     end
-    return true
+    return is_valid(layout.content)
 end
 
 Base.length(layout::ListOffsetArray) = length(layout.offsets) - 1
@@ -241,21 +259,33 @@ function Base.getindex(layout::ListOffsetArray, i::Int)
     layout.content[start:stop]
 end
 
-function Base.getindex(layout::ListOffsetArray, r::UnitRange{Int})
-    ListOffsetArray(layout.offsets[(r.start):(r.stop+1)], layout.content)
-end
+Base.getindex(layout::ListOffsetArray, r::UnitRange{Int}) = ListOffsetArray(
+    layout.offsets[(r.start):(r.stop+1)],
+    layout.content,
+    parameters = layout.parameters,
+    behavior = typeof(layout).parameters[end],
+)
+
+Base.getindex(layout::ListOffsetArray, f::Symbol) = ListOffsetArray(
+    layout.offsets,
+    layout.content[f],
+    parameters = layout.parameters,
+    behavior = typeof(layout).parameters[end],
+)
 
 function Base.:(==)(layout1::ListOffsetArray, layout2::ListOffsetArray)
     if length(layout1) != length(layout2)
         return false
-    else
-        for (x, y) in zip(layout1, layout2)
-            if x != y
-                return false
-            end
-        end
-        return true
     end
+    if !compatible(layout1.parameters, layout2.parameters)
+        return false
+    end
+    for (x, y) in zip(layout1, layout2)
+        if x != y
+            return false
+        end
+    end
+    return true
 end
 
 function end_list!(layout::ListOffsetArray)
@@ -284,6 +314,135 @@ function Base.getindex(
     i::Int,
 ) where {INDEX<:IndexBig,BUFFER<:AbstractVector{UInt8}}
     getindex(ListOffsetArray(layout.offsets, PrimitiveArray(layout.content.data)), i).data
+end
+
+### RecordArray ##########################################################
+
+mutable struct RecordArray{CONTENTS<:NamedTuple,BEHAVIOR} <: Content{BEHAVIOR}
+    const contents::CONTENTS
+    length::Int64
+    const parameters::Parameters
+    RecordArray(
+        contents::CONTENTS,
+        length::Int64;
+        parameters::Parameters = Parameters(),
+        behavior::Symbol = :default,
+    ) where {CONTENTS<:NamedTuple} = new{CONTENTS,behavior}(contents, length, parameters)
+end
+
+RecordArray(
+    contents::CONTENTS;
+    parameters::Parameters = Parameters(),
+    behavior::Symbol = :default,
+) where {CONTENTS<:NamedTuple} = RecordArray(
+    contents,
+    minimum(if length(contents) == 0
+        0
+    else
+        [length(x) for x in contents]
+    end),
+    parameters = parameters,
+    behavior = behavior,
+)
+
+struct Record{ARRAY<:RecordArray}
+    array::ARRAY
+    at::Int64
+end
+
+function copy(
+    layout::RecordArray{CONTENTS,BEHAVIOR};
+    contents::Union{Unset,CONTENTS} = Unset(),
+    length::Union{Unset,Int64} = Unset(),
+    parameters::Union{Unset,Parameters} = Unset(),
+    behavior::Union{Unset,Symbol} = Unset(),
+) where {CONTENTS<:NamedTuple,BEHAVIOR}
+    if isa(contents, Unset)
+        contents = layout.contents
+    end
+    if isa(length, Unset)
+        length = layout.length
+    end
+    if isa(parameters, Unset)
+        parameters = layout.parameters
+    end
+    if isa(behavior, Unset)
+        behavior = typeof(layout).parameters[end]
+    end
+    RecordArray(contents, length, parameters = parameters, behavior = behavior)
+end
+
+function is_valid(layout::RecordArray)
+    for x in values(layout.contents)
+        if length(x) < layout.length
+            return false
+        end
+        if !is_valid(x)
+            return false
+        end
+    end
+    return true
+end
+
+Base.length(layout::RecordArray) = layout.length
+Base.firstindex(layout::RecordArray) = 1
+Base.lastindex(layout::RecordArray) = layout.length
+
+Base.getindex(layout::RecordArray, i::Int) = Record(layout, i)
+
+Base.getindex(layout::RecordArray, r::UnitRange{Int}) =
+    RecordArray{typeof(layout).parameters[end]}(
+        NamedTuple{keys(layout.contents)}(
+            Pair(k, v[r]) for (k, v) in pairs(layout.contents)
+        ),
+        min(r.stop, layout.length) - max(r.start, 1) + 1,   # unnecessary min/max
+        layout.parameters,
+    )
+
+function Base.getindex(layout::RecordArray, f::Symbol)
+    content = layout.contents[f]
+    content[firstindex(content):firstindex(content)+length(layout)-1]
+end
+
+Base.getindex(layout::Record, f::Symbol) = layout.array.contents[f][layout.at]
+
+function Base.:(==)(
+    layout1::RecordArray{CONTENTS},
+    layout2::RecordArray{CONTENTS},
+) where {CONTENTS<:NamedTuple}
+    if length(layout1) != length(layout2)
+        return false
+    end
+    if !compatible(layout1.parameters, layout2.parameters)
+        return false
+    end
+    for k in keys(layout1.contents)   # same keys because same CONTENTS type
+        if layout1[k] != layout2[k]   # compare whole arrays
+            return false
+        end
+    end
+    return true
+end
+
+function Base.:(==)(
+    layout1::Record{ARRAY},
+    layout2::Record{ARRAY},
+) where {CONTENTS<:NamedTuple,ARRAY<:RecordArray{CONTENTS}}
+    if !compatible(layout1.array.parameters, layout2.array.parameters)
+        return false
+    end
+    for k in keys(layout1.array.contents)   # same keys because same CONTENTS type
+        if layout1[k] != layout2[k]         # compare record items
+            return false
+        end
+    end
+    return true
+end
+
+function end_record!(layout::RecordArray)
+    layout.length += 1
+    @assert all(length(x) >= layout.length for x in layout.contents)
+    layout
 end
 
 end
