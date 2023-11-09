ac22346a3472c90411e23e86300092cfe4a2322c
Ianna Osborne <ianna.osborne@cern.ch>
2023-11-09 10:56:38 +0100

    try reexport AwkwardArray

--- src/pycall/AwkwardPyCall.jl
+++ src/pycall/AwkwardPyCall.jl
@@ -1,19 +1,7 @@
 module AwkwardPyCall
 
-<<<<<<< HEAD
 using PyCall
 using JSON
-=======
-if !isdefined(Main, :PyCall)
-    println("PyCall is not installed. Installing...")
-
-    import Pkg
-
-    Pkg.add("PyCall")
-end
-    
-using PyCall
->>>>>>> bd3c0dd (Update and rename julia_helpers.jl to AwkwardPyCall.jl)
 using AwkwardArray
 
 const ak = pyimport("awkward")
@@ -21,33 +9,20 @@ const np = pyimport("numpy")
 
 function _as_numpy(array::AbstractVector{UInt8})
     py_array = PyObject(array)
-<<<<<<< HEAD
     np.asarray(py_array, dtype = np.uint8)
-=======
-    np.asarray(py_array, dtype=np.uint8)
->>>>>>> bd3c0dd (Update and rename julia_helpers.jl to AwkwardPyCall.jl)
 end
 
 function julia_array_to_python(array)
     form, len, containers = AwkwardArray.to_buffers(array)
-<<<<<<< HEAD
 
     py_buffers = Dict{String,Any}()
 
     for (key, buffer) in containers
         py_buffers[key] = _as_numpy(buffer)
-=======
-    
-    py_buffers = Dict{String, Any}()
-
-    for (key, buffer) in containers
-        numpy_arrays[key] = _as_numpy(buffer)
->>>>>>> bd3c0dd (Update and rename julia_helpers.jl to AwkwardPyCall.jl)
     end
 
     return ak.from_buffers(form, len, py_buffers)
 end
-<<<<<<< HEAD
 
 function _as_julia(py_buffer)
     uint8_buffer = reinterpret(UInt8, py_buffer)
@@ -66,5 +41,3 @@ function python_array_to_julia(py_array)
 end
 
 end # module
-=======
->>>>>>> bd3c0dd (Update and rename julia_helpers.jl to AwkwardPyCall.jl)
