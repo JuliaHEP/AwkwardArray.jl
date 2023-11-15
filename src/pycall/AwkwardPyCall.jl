76dcd25cf09947ed2f2bc6e4579ef631ebb3040d
Ianna Osborne <ianna.osborne@cern.ch>
2023-11-15 18:29:27 +0100

    feat: add python to julia functions

--- src/pycall/AwkwardPyCall.jl
+++ src/pycall/AwkwardPyCall.jl
@@ -25,10 +25,26 @@ function julia_array_to_python(array)
     py_buffers = Dict{String, Any}()
 
     for (key, buffer) in containers
-        numpy_arrays[key] = _as_numpy(buffer)
+        py_buffers[key] = _as_numpy(buffer)
     end
 
     return ak.from_buffers(form, len, py_buffers)
+end
+
+function _as_julia(py_buffer)
+    uint8_buffer = reinterpret(UInt8, py_buffer)
+    return uint8_buffer
+end
+
+function python_array_to_julia(py_array)
+    form, len, containers = ak.to_buffers(py_array)
+
+    julia_buffers = Dict{String, AbstractVector{UInt8}}()
+    for (key, buffer) in containers
+        julia_buffers[key] = _as_julia(buffer)
+    end
+
+    return AwkwardArray.from_buffers(form.to_json(), len, julia_buffers)
 end
 
 end # module
