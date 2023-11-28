b75414dea12fed67f47bb81fb46c9919a83d042e
Ianna Osborne <ianna.osborne@cern.ch>
2023-11-28 22:30:49 +0100

    fix: merge rename to convert branch (#49)

--- ext/AwkwardPythonCallExt/AwkwardPythonCallExt.jl
+++ ext/AwkwardPythonCallExt/AwkwardPythonCallExt.jl
@@ -3,38 +3,33 @@ using PythonCall
 using JSON
 import AwkwardArray
 
-function _as_numpy(array::AbstractVector{UInt8})
-    np = pyimport("numpy")
-    np.asarray(array, dtype = np.uint8)
-end
-
-function AwkwardArray.julia_array_to_python(array)
-    form, len, containers = AwkwardArray.to_buffers(array)
+function AwkwardArray.convert(layout::AwkwardArray.Content)::Py
+    form, len, containers = AwkwardArray.to_buffers(layout)
 
     py_buffers = Dict{String,Any}()
 
     for (key, buffer) in containers
-        py_buffers[key] = _as_numpy(buffer)
+        py_buffers[key] = pyimport("numpy").asarray(buffer, dtype = pyimport("numpy").uint8)
     end
 
-    return pyimport("awkward").from_buffers(form, len, py_buffers)
+    pyimport("awkward").from_buffers(form, len, py_buffers)
 end
 
-function _as_julia(py_buffer)
-    uint8_buffer = reinterpret(UInt8, py_buffer)
-    return uint8_buffer
-end
-
-function AwkwardArray.python_array_to_julia(py_array)
-    form, len, _containers = pyimport("awkward").to_buffers(py_array)
+function AwkwardArray.convert(array::Py)::AwkwardArray.Content
+    form, len, _containers = pyimport("awkward").to_buffers(array)
     containers = pyconvert(Dict, _containers)
 
     julia_buffers = Dict{String,AbstractVector{UInt8}}()
+
     for (key, buffer) in containers
-        julia_buffers[key] = _as_julia(buffer)
+        julia_buffers[key] = reinterpret(UInt8, buffer)
     end
 
-    return AwkwardArray.from_buffers(pyconvert(String, form.to_json()), pyconvert(Int, len), julia_buffers)
+    AwkwardArray.from_buffers(
+        pyconvert(String, form.to_json()),
+        pyconvert(Int, len),
+        julia_buffers,
+    )
 end
 
 end # module
