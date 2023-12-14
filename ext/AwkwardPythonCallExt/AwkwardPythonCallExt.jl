5ab43352bd424998cbfc60c652aafbf0ac95cc97
Ianna Osborne <ianna.osborne@cern.ch>
2023-12-14 16:59:10 +0100

    fix: update conversion rules

--- ext/AwkwardPythonCallExt/AwkwardPythonCallExt.jl
+++ ext/AwkwardPythonCallExt/AwkwardPythonCallExt.jl
@@ -49,17 +49,12 @@ function AwkwardArray.convert(array::PyIterable{Any})::AwkwardArray.Content
 end
 
 
+
 PythonCall.pyconvert_rule_array_nocopy(::Type{AwkwardArray.PrimitiveArray}, x::Py) = AwkwardArray.convert(x)
 PythonCall.pyconvert_rule_array_nocopy(::Type{AwkwardArray.EmptyArray}, x::Py) = AwkwardArray.convert(x)
 PythonCall.pyconvert_rule_array_nocopy(::Type{AwkwardArray.ListOffsetArray}, x::Py) = AwkwardArray.convert(x)
 PythonCall.pyconvert_rule_array_nocopy(::Type{AwkwardArray.ListArray}, x::Py) = AwkwardArray.convert(x)
 PythonCall.pyconvert_rule_array_nocopy(::Type{AwkwardArray.RegularArray}, x::Py) = AwkwardArray.convert(x)
-PythonCall.pyconvert_rule_array_nocopy(::Type{AwkwardArray.StringOffsetArray}, x::Py) = AwkwardArray.convert(x)
-PythonCall.pyconvert_rule_array_nocopy(::Type{AwkwardArray.StringArray}, x::Py) = AwkwardArray.convert(x)
-PythonCall.pyconvert_rule_array_nocopy(::Type{AwkwardArray.StringRegularArray}, x::Py) = AwkwardArray.convert(x)
-PythonCall.pyconvert_rule_array_nocopy(::Type{AwkwardArray.ByteStringOffsetArray}, x::Py) = AwkwardArray.convert(x)
-PythonCall.pyconvert_rule_array_nocopy(::Type{AwkwardArray.ByteStringArray}, x::Py) = AwkwardArray.convert(x)
-PythonCall.pyconvert_rule_array_nocopy(::Type{AwkwardArray.ByteStringRegularArray}, x::Py) = AwkwardArray.convert(x)
 PythonCall.pyconvert_rule_array_nocopy(::Type{AwkwardArray.RecordArray}, x::Py) = AwkwardArray.convert(x)
 PythonCall.pyconvert_rule_array_nocopy(::Type{AwkwardArray.TupleArray}, x::Py) = AwkwardArray.convert(x)
 PythonCall.pyconvert_rule_array_nocopy(::Type{AwkwardArray.IndexedArray}, x::Py) = AwkwardArray.convert(x)
@@ -74,12 +69,6 @@ PythonCall.pyconvert_add_rule("awkward.highlevel:Array", AwkwardArray.EmptyArray
 PythonCall.pyconvert_add_rule("awkward.highlevel:Array", AwkwardArray.ListOffsetArray, PythonCall.pyconvert_rule_array_nocopy, PythonCall.PYCONVERT_PRIORITY_ARRAY)
 PythonCall.pyconvert_add_rule("awkward.highlevel:Array", AwkwardArray.ListArray, PythonCall.pyconvert_rule_array_nocopy, PythonCall.PYCONVERT_PRIORITY_ARRAY)
 PythonCall.pyconvert_add_rule("awkward.highlevel:Array", AwkwardArray.RegularArray, PythonCall.pyconvert_rule_array_nocopy, PythonCall.PYCONVERT_PRIORITY_ARRAY)
-# FIXME: PythonCall.pyconvert_add_rule("awkward.highlevel:Array", AwkwardArray.StringOffsetArray, PythonCall.pyconvert_rule_array_nocopy, PythonCall.PYCONVERT_PRIORITY_ARRAY)
-# FIXME: PythonCall.pyconvert_add_rule("awkward.highlevel:Array", AwkwardArray.StringArray, PythonCall.pyconvert_rule_array_nocopy, PythonCall.PYCONVERT_PRIORITY_ARRAY)
-# FIXME: PythonCall.pyconvert_add_rule("awkward.highlevel:Array", AwkwardArray.StringRegularArray, PythonCall.pyconvert_rule_array_nocopy, PythonCall.PYCONVERT_PRIORITY_ARRAY)
-# FIXME: PythonCall.pyconvert_add_rule("awkward.highlevel:Array", AwkwardArray.ByteStringOffsetArray, PythonCall.pyconvert_rule_array_nocopy, PythonCall.PYCONVERT_PRIORITY_ARRAY)
-# FIXME: PythonCall.pyconvert_add_rule("awkward.highlevel:Array", AwkwardArray.ByteStringArray, PythonCall.pyconvert_rule_array_nocopy, PythonCall.PYCONVERT_PRIORITY_ARRAY)
-# FIXME: PythonCall.pyconvert_add_rule("awkward.highlevel:Array", AwkwardArray.ByteStringRegularArray, PythonCall.pyconvert_rule_array_nocopy, PythonCall.PYCONVERT_PRIORITY_ARRAY)
 PythonCall.pyconvert_add_rule("awkward.highlevel:Array", AwkwardArray.RecordArray, PythonCall.pyconvert_rule_array_nocopy, PythonCall.PYCONVERT_PRIORITY_ARRAY)
 PythonCall.pyconvert_add_rule("awkward.highlevel:Array", AwkwardArray.TupleArray, PythonCall.pyconvert_rule_array_nocopy, PythonCall.PYCONVERT_PRIORITY_ARRAY)
 PythonCall.pyconvert_add_rule("awkward.highlevel:Array", AwkwardArray.IndexedArray, PythonCall.pyconvert_rule_array_nocopy, PythonCall.PYCONVERT_PRIORITY_ARRAY)
