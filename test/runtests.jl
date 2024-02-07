fba439ead77da261ca14993dedbce238f0a3bb21
Ianna Osborne <ianna.osborne@cern.ch>
2024-02-07 11:07:37 +0100

    fix: add a test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2642,13 +2642,13 @@ end
     )
 end
 
-# testset "from_iter     # 16" begin
-#     @test AwkwardArray.is_valid(
-#         AwkwardArray.from_iter(
-#             Vector{Union{Missing,Float64,Vector{Int64}}}([1.1, [1, 2], missing, [3]]),
-#         ),
-#     )
-# end
+@testset "from_iter     # 16" begin
+    @test AwkwardArray.is_valid(
+        AwkwardArray.from_iter(
+            Vector{Union{Missing,Float64,Vector{Int64}}}([1.1, [1, 2], missing, [3]]),
+        ),
+    )
+end
 
 # testset "from_iter     # 17" begin
 #     @test AwkwardArray.is_valid(
