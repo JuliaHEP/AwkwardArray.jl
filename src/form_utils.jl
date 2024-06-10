e712951dad8c138210c4fd931f976235e2c64669
Ianna Osborne <ianna.osborne@cern.ch>
2024-06-10 20:58:08 +0200

    add check on iterable type

--- src/form_utils.jl
+++ src/form_utils.jl
@@ -1,5 +1,35 @@
 using AwkwardArray
 
+is_iterable(obj) = try
+    iterate(obj) !== nothing
+catch
+    false
+end
+
+# Function to check if a type is iterable
+function is_iterable_type(T::Type)
+    try
+        iterate(T())
+        return true
+    catch
+        return false
+    end
+end
+
+function object_type_iterable(obj)
+    # Get the type of the object
+    T = typeof(obj)
+    
+    # Create an instance of the specified type
+    obj_instance = T()
+    
+    # Attempt to iterate over the object
+    iter_result = iterate(obj_instance)
+    
+    # Return true if iteration succeeds, false otherwise
+    return iter_result !== nothing
+end
+
 function type_to_form(::Type{T}, form_key_id::Int64) where {T}
     # Helper function to generate the form key string
     function generate_form_key!(form_key_id_ref)
@@ -23,9 +53,31 @@ function type_to_form(::Type{T}, form_key_id::Int64) where {T}
     elseif T <: Complex
         return "{\"class\": \"NumpyArray\", \"primitive\": \"" * string(T) *
                "\", \"form_key\": \"" * form_key * "\"}"
+    end
+
+    # Check if the type T is iterable
+    if is_iterable_type(T)
+        parameters = ""
+        
+        # Get the value type of the iterable T
+        value_type = eltype(T)
+        
+        if value_type == Char
+            parameters = " \"parameters\": { \"__array__\": \"string\" }, "
+        end
+        
+        form_key = "node$(form_key_id)"
+        
+        offsets = "offsets_placeholder" # Replace this with the actual logic to get offsets
+        
+        content = type_to_form(value_type, form_key_id + 1) # Recursive call for value_type
+        
+        return "{\"class\": \"ListOffsetArray\", \"offsets\": \"$offsets\", \"content\": $content, $parameters \"form_key\": \"$form_key\"}"
     else
         error("Type not supported")
     end
+    
+    error("Type not supported")
 end
 
 function type_to_form(::Type{T}) where T <: Union{Int, Float64, String, Bool}
