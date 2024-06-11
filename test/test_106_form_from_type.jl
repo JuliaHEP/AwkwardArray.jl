9974d57261f570ecc908bfa49792bbee99888054
Ianna Osborne <ianna.osborne@cern.ch>
2024-06-11 13:00:51 +0200

    add module

--- test/test_106_form_from_type.jl
+++ test/test_106_form_from_type.jl
@@ -2,7 +2,7 @@ using AwkwardArray
 
 @testset "Form from type" begin
     begin
-        form = type_to_form(Int, 1)
+        form = AwkwardArray.type_to_form(Int, 1)
         @test form == """{"class": "NumpyArray", "primitive": "int64", "form_key": "node1"}"""
     end
 end
